330c2b7cd1621c3f9d248ccc0fc151f52522d5ad
HSH <han41562@gmail.com>
2024-08-10 15:10:41 +0900

    test thread pool's maxThreads

---
+++ src/main/java/com/example/springplayground/connectionPool/CheckConnectionPoolController.java
@@ -0,0 +1,35 @@
+package com.example.springplayground.connectionPool;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class CheckConnectionPoolController {
+
+    private AtomicInteger n = new AtomicInteger(0);
+
+    private ReentrantLock lock = new ReentrantLock();
+
+    @GetMapping("/test")
+    public void getConnectionPoolInfo() throws InterruptedException {
+        int i = n.incrementAndGet();
+        log.info("Connection pool test + " + i);
+        Thread.sleep(5000);
+        log.info("Connection pool test end - " + i);
+    }
+
+    @GetMapping("/test2")
+    public void getConnectionPoolInfo2() throws InterruptedException {
+        lock.unlock();
+    }
+
+}
---
+++ src/test/java/com/example/springplayground/connectionPool/CheckConnectionPoolControllerTest.java
@@ -0,0 +1,47 @@
+package com.example.springplayground.connectionPool;
+
+import com.example.springplayground.TestHttpUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.http.HttpResponse;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CheckConnectionPoolControllerTest {
+    static AtomicInteger count = new AtomicInteger(0);
+    @Test
+    void test() throws Exception {
+        final var NUMBER_OF_THREAD = 10;
+        var threads = new Thread[NUMBER_OF_THREAD];
+
+        for (int i = 0; i < NUMBER_OF_THREAD; i++) {
+            threads[i] = new Thread(() -> incrementIfOk(TestHttpUtils.send("/test")));
+        }
+
+        for (final var thread : threads) {
+            thread.start();
+            //Thread.sleep(50);
+        }
+
+        for (final var thread : threads) {
+            thread.join();
+        }
+
+        assertThat(count.intValue()).isEqualTo(2);
+    }
+
+    private static void incrementIfOk(final HttpResponse<String> response) {
+        if (response.statusCode() == 200) {
+            count.incrementAndGet();
+        }
+    }
+
+    private static void printResponseCode(final HttpResponse<String> response) {
+        System.out.println(response.statusCode());
+    }
+
+}
