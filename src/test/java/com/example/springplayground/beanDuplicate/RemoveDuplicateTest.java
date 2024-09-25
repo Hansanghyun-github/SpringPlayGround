4492f4365f89e0e366a0c4f317c9b1dfa1dd308d
HSH <han41562@gmail.com>
2024-09-25 12:42:54 +0900

    add method of TestConfiguration

--- src/test/java/com/example/springplayground/beanDuplicate/RemoveDuplicateTest.java
+++ src/test/java/com/example/springplayground/beanDuplicate/RemoveDuplicateTest.java
@@ -1,6 +1,8 @@
 package com.example.springplayground.beanDuplicate;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Import(MyTestConfiguration.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class RemoveDuplicateTest {
     @Autowired
     EmptyInterface emptyInterface;
@@ -18,16 +21,23 @@ public class RemoveDuplicateTest {
     ApplicationContext applicationContext;
     
     @Test
-    void success_to_bean_create() throws Exception {
+    void 우선순위로_인해_빈_주입에_성공한다() throws Exception {
         // when // then
         emptyInterface.print();
     }
-    
+
     @Test
-    void duplicate_bean_problem_even_if_apply_TestConfiguration() throws Exception {
+    void 스프링_빈은_2개_조회되서_실패한다() throws Exception {
         // when // then
         Assertions.assertThatThrownBy(() -> applicationContext.getBean(EmptyInterface.class))
                 .isInstanceOf(NoUniqueBeanDefinitionException.class);
     }
-    
+
+    @Test
+    void 중복되는_빈들_조회() throws Exception {
+        // when // then
+        applicationContext.getBeansOfType(EmptyInterface.class)
+                .forEach((k, v) -> System.out.println(k + " : " + v));
+    }
+
 }
